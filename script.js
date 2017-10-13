82fb2b87b3048b320bc5b829a992f4d788c1bf73
BritishPandaChick <sbartley1080@gmail.com>
2017-10-12 20:48:51 -0400

    Debug code files

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $(document).keydown(function(key){
+    switch(parseInt(key.which, 10)) {
+      //left arrow key pressed
+      case 37:
+      $('img').animate({left: "-=10px"}, 'fast');
+      break;
+
+      //Up arrow pressed
+      case 38:
+      $('img').animate({right: "-=10px"}, 'fast');
+      break;
+
+      //Right arrow pressed
+      case 39:
+      $('img').animate({left: "+=10px"}, 'fast');
+      break;
+      
+      //Down arrow pressed
+      case 40:
+      $('img').animate({top: "+=10px"}, 'fast');
+      break;
+    }
+  });
+});
